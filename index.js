6795b2510593fa46f62234ab47c1fbcbc3a87481
Simon Taylor <nizmox@gmail.com>
2015-02-06 14:49:40 +1100

    change spaces to tabs

--- index.js
+++ index.js
@@ -14,14 +14,14 @@ app.use(lessMiddleware(__dirname + '/public'));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-  res.render('site/home', {});
+	res.render('site/home', {});
 });
 
 var server = app.listen(3000, function () {
 
-  var host = server.address().address
-  var port = server.address().port
+	var host = server.address().address
+	var port = server.address().port
 
-  console.log('Example app listening at http://%s:%s', host, port)
+	console.log('Example app listening at http://%s:%s', host, port)
 
 })
